4f2e74ff6409950bf335cf1b59cd455436e90f6f
NazmusSakib <n.sakib.sad@gmail.com>
2024-07-11 02:26:53 +0600

    added categories

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
+//  --------   category section   ----------
 
+const loadcategory = async() => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+  displaycategory(data.data.news_category);
+}
+loadcategory();
+const displaycategory = categories => {
+  const categoryContainer = document.getElementById('category-container');
+
+  for(const category of categories){
+    // console.log(category);
+    const newCategory = document.createElement('div');
+    newCategory.innerHTML = `
+      <h4 onclick="loadCategoryDetails('${category.category_id}')"> ${category.category_name} </h4>
+    `
+    categoryContainer.appendChild(newCategory);
+  }
+}
+
+// ----------- category details  --------------
+
+const loadCategoryDetails = categoryId => {
+  console.log(categoryId);
+}
